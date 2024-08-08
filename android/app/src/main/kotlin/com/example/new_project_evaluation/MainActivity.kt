ff9838c18b821e9c9cba685edd9796bc58d62fba
hasnain <aliabbas191218@gmail.com>
2024-08-08 17:01:51 +0500

    First Commit 1

--- android/app/src/main/kotlin/com/example/new_project_evaluation/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/new_project_evaluation/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.new_project_evaluation
 
-import io.flutter.embedding.android.FlutterActivity
-
 class MainActivity: FlutterActivity()
+
+open class FlutterActivity {
+
+}
--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -43,7 +43,7 @@ class HomeScreen extends StatelessWidget {
               contentPadding: EdgeInsets.all(16),
               title: Text(
                 items[index],
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500,color: Colors.white),
               ),
               onTap: () {
                 ScaffoldMessenger.of(context).showSnackBar(
